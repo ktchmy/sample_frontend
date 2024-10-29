2256bd2d81ddc10463bdf3c4173ce17571a8a791
Cheng Ka Teng <jarvis@Mathews-Laptop.local>
2024-10-29 14:29:07 +0800

    update README and url refactor

--- src/app/list-submission.service.ts
+++ src/app/list-submission.service.ts
@@ -6,40 +6,36 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class ListSubmissionService {
-  url = "https://sample_backend.test/api/sample-submission/list";
-  createUrl = "https://sample_backend.test/api/sample-submission/add";
-  getUrl = "https://sample_backend.test/api/sample-submission/get/";
-  updateUrl = "https://sample_backend.test/api/sample-submission/update/";
-  deleteUrl = "https://sample_backend.test/api/sample-submission/delete/";
+  url = "https://sample_backend.test/api/sample-submission/";
   constructor(private http: HttpClient) { }
   getSubmissions(): Observable<any>{
     const headers = new HttpHeaders({ 'Accept':'application/json' });
-    return this.http.get(this.url, {headers});
+    return this.http.get(this.url+'list', {headers});
   }
 
   createSubmission(submission: any): Observable<any>{
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded','Accept':'application/json' });
     //Change from Json to x-form-encode to pass thru
     const body = 'name=' + submission.name + '&email=' + submission.email +'&message=' + submission.message + '&file=' + submission.file;
-    return this.http.post(this.createUrl,  body, {headers});
+    return this.http.post(this.url+'add',  body, {headers});
   }
 
   getSubmission(id:number): Observable<any>{
     const headers = new HttpHeaders({ 'Accept':'application/json' });
-    return this.http.get(this.getUrl+id, {headers});
+    return this.http.get(this.url+'get/'+id, {headers});
   }
 
   updateSubmission(id: number, submission: any): Observable<any>{
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded','Accept':'application/json' });
     //Change from Json to x-form-encode to pass thru
     const body = 'id=' + id + '&name=' + submission.name + '&email=' + submission.email +'&message=' + submission.message + '&file=' + submission.file;
-    return this.http.post(this.updateUrl+id,  body, {headers});
+    return this.http.post(this.url+'update/'+id,  body, {headers});
   }
 
   deleteSubmission(id: number, submission: any): Observable<any>{
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded','Accept':'application/json' });
     //Change from Json to x-form-encode to pass thru
     const body = 'id=' + id + '&name=' + submission.name + '&email=' + submission.email +'&message=' + submission.message + '&file=' + submission.file;
-    return this.http.post(this.deleteUrl+id,  body, {headers});
+    return this.http.post(this.url+'delete/'+id,  body, {headers});
   }
 }
