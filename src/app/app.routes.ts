575e74aefa47c36b2ca962d3ca6b3dd497f38dfc
Cheng Ka Teng <jarvis@Mathews-Laptop.local>
2024-10-29 11:15:59 +0800

    MINOR: change list of submission to be show on homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import {SubmissionComponent} from "./submission/submission.component";
 import {DetailsComponent} from "./details/details.component";
 
 export const routes: Routes = [
-  { path: '', component: AppComponent},
+  { path: '', component: DashboardComponent},
   { path: 'dashboard', component: DashboardComponent, title:'List Submissions' },
   { path: 'submission', component: SubmissionComponent },
   { path: 'details/:id', component: DetailsComponent },
